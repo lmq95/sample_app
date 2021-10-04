449c10e408f16149e81f2b41ecb0a1ec07e75a20
MaoTorihata <4321namap@gmai.com>
2021-10-04 05:37:37 +0000

    [finish]3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,12 +5,12 @@ class TodolistsController < ApplicationController
   
   
   def create
-    # １. データを新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # ３. トップ画面へリダイレクト
-    redirect_to todolist_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
   end
   
   def index
@@ -30,14 +30,11 @@ class TodolistsController < ApplicationController
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  
-  def create
-    @list = List.new(list_params)
-    if @list.save
-      redirect_to todolist_path(@list.id)
-    else
-      redirect_to todolists_new_path
-    end
+ 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to todolists_path
   end
   
   
